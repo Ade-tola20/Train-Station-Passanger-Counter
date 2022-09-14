3d8275bd9975d4d91677ea1339eaf4be70ef4d9d
Oduwole Faruq Adetola <104711298+Ade-tola20@users.noreply.github.com>
2022-09-14 03:41:35 -0700

    Add files via upload

---
+++ train.js
@@ -0,0 +1,16 @@
+let saveEl = document.getElementById("save-el")
+let countEl =  document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+    count = count + 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countString = count + " - "
+    saveEl.textContent += countString 
+    count = 0
+    countEl.textContent = count
+}
